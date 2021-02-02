d52b2ea24e4a30770cf657c883f90b7f5c09f2a3
Cyril Vernhes <c.vernhes3@gmail.com>
2021-02-02 16:13:28 +0100

    add travis files

--- pur_beurre/urls.py
+++ pur_beurre/urls.py
@@ -17,8 +17,13 @@ from django.contrib import admin
 from django.urls import include, path
 
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path('', include('substitute.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('sentry-debug/', trigger_error),
 ]
